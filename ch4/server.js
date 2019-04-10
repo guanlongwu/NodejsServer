8bc009960c403ca515df2f9a852595962565efe5
guanlongwu <guanlongwu@qq.com>
2019-04-10 16:28:53 +0800

    This is a suggested server built by nodejs for client developers to learn the websocket protocol.

---
+++ ch4/server.js
@@ -0,0 +1,25 @@
+﻿var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/jquery-1.12.3.min.js', function (req, res) {
+    res.sendFile(__dirname + '/js/jquery-1.12.3.min.js');
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg) {
+        console.log("msg:" + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
---
+++ ch5/server.js
@@ -0,0 +1,77 @@
+﻿var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var uuid = require('node-uuid');
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/client.html');
+});
+app.get('/jquery.js', function (req, res) {
+    res.sendFile(__dirname + '/js/jquery-1.12.3.min.js');
+});
+app.get('/bootstrap.min.css', function (req, res) {
+    res.sendFile(__dirname + '/bootstrap-3.3.5/css/bootstrap.min.css');
+});
+app.get('/bootstrap.min.js', function (req, res) {
+    res.sendFile(__dirname + '/bootstrap-3.3.5/js/bootstrap.min.js');
+});
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+function wsSend(type, client_uuid, nickname, message) {
+    io.emit(type, JSON.stringify({
+        "type": type,
+        "id": client_uuid,
+        "nickname": nickname,
+        "message": message
+    }));
+}
+var clientIndex = 1;
+var clients = [];
+io.on('connection', function (socket) {
+    var client_uuid = uuid.v4();
+    var nickname = "SocketUser" + clientIndex;
+    clientIndex += 1;
+    clients.push({ "id": client_uuid, "nickname": nickname });
+    console.log('client [%s] connected', client_uuid);
+    var connect_message = nickname + " has connected";
+    wsSend("notification", client_uuid, nickname, connect_message);
+    console.log('client [%s] connected', client_uuid);
+    socket.on('message', function (message) {
+        console.log(message);
+        wsSend("message", client_uuid, nickname, message);
+       
+    });
+    socket.on('nick_update', function (nick) {
+        var old_nickname = nickname;
+        nickname = nick;
+        var nickname_message = "Client " + old_nickname + " changed to " + nickname;
+        wsSend("nick_update", client_uuid, nickname, nickname_message);
+    });
+
+    var closeSocket = function (customMessage) {
+        for (var i = 0; i < clients.length; i++) {
+            if (clients[i].id == client_uuid) {
+                var disconnect_message;
+                if (customMessage) {
+                    disconnect_message = customMessage;
+                } else {
+                    disconnect_message = nickname + " has disconnected";
+                }
+                wsSend("notification", client_uuid, nickname, disconnect_message);
+                clients.splice(i, 1);
+            }
+        }
+    };
+    socket.on('disconnect', function () {
+        closeSocket();
+    });
+    process.on('SIGINT', function () {
+        console.log("Closing things");
+        closeSocket('Server has disconnected');
+        process.exit();
+    });
+    
+ 
+});
+ 
